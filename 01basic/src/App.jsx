6104d74b640074c6c28a9f27b69cd4b31800fb39
Saksham Thakur <saksham1448.be23@chitkarauniversity.edu.in>
2025-03-14 22:18:54 +0530

    03 Video_CC

--- 01basic/src/App.jsx
+++ 01basic/src/App.jsx
@@ -1,9 +1,12 @@
+import Chai from "./Chai";
+
 function App() {
 
   return (
-    
-       <h1>React with vite | SAKSHAM </h1>
-       
+    <>
+      <Chai />
+      <h1>Hello !!</h1>
+    </>
   )
 }
 
---
+++ 01basic/src/Chai.jsx
@@ -0,0 +1,6 @@
+function Chai(){
+    return(
+        <h3>Saksham is Ready</h3>
+    )
+}
+export default Chai;
