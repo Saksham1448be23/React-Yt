fdc3b5fc60887839a76083e011e6f17238f8dd96
Saksham Thakur <saksham1448.be23@chitkarauniversity.edu.in>
2025-03-15 13:08:35 +0530

    03

--- 01basic/src/main.jsx
+++ 01basic/src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).
-render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+function Hello() {
+  return (
+    <>
+      <h1>EK aur Function</h1>
+    </>
+  )
+}
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit google site</a>
+);
+
+const reactElement = React.createElement('a', {
+  href: 'https://google.com',
+  target: '_blank',
+}, 'Visit my React google site'
 )
+
+ReactDOM.createRoot(document.getElementById('root')).
+  render(
+    <React.StrictMode>
+      {reactElement}
+    </React.StrictMode>,
+  )
